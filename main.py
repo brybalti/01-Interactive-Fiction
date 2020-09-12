18a803fe6858a4dda88bcd2e0e34235370c80dda
brybalti <brybalti@iu.edu>
2020-09-12 19:43:27 -0400

    First project in Twine

---
+++ main.py
@@ -0,0 +1,324 @@
+{
+  "passages": [
+    {
+      "text": "You awake in a room feeling dizzy and no idea how you got here. You take in your surroundings.\n\nYou are in a room with two portals and a locked door. You can go through either the blue portal or the yellow portal:\n\n[[Red Portal->Next]]\n[[Yellow Portal->Hint Room]]",
+      "links": [
+        {
+          "name": "Red Portal",
+          "link": "Next",
+          "pid": "3"
+        },
+        {
+          "name": "Yellow Portal",
+          "link": "Hint Room",
+          "pid": "2"
+        }
+      ],
+      "name": "Start",
+      "pid": "1",
+      "position": {
+        "x": "828",
+        "y": "177"
+      }
+    },
+    {
+      "text": "There are no other doors in this room, just something written on the wall. Its an image of a Finish Line with and equals sign and a rainbow.",
+      "name": "Hint Room",
+      "pid": "2",
+      "position": {
+        "x": "964",
+        "y": "179"
+      }
+    },
+    {
+      "text": "In this room you see three other portals:\n\n[[Orange Portal->Path#2]]\n[[Cyan Portal->Side#1]]\n[[Magenta Portal->Side#2]]",
+      "links": [
+        {
+          "name": "Orange Portal",
+          "link": "Path#2",
+          "pid": "6"
+        },
+        {
+          "name": "Cyan Portal",
+          "link": "Side#1",
+          "pid": "4"
+        },
+        {
+          "name": "Magenta Portal",
+          "link": "Side#2",
+          "pid": "5"
+        }
+      ],
+      "name": "Next",
+      "pid": "3",
+      "position": {
+        "x": "828",
+        "y": "312"
+      }
+    },
+    {
+      "text": "This room has cobwebs and one white portal.\n\n[[White Portal->Side#1.2]]",
+      "links": [
+        {
+          "name": "White Portal",
+          "link": "Side#1.2",
+          "pid": "7"
+        }
+      ],
+      "name": "Side#1",
+      "pid": "4",
+      "position": {
+        "x": "698",
+        "y": "313"
+      }
+    },
+    {
+      "text": "You only see a white portal straight in front of you and through the portal you can see a hanging light.\n\n[[Portal->Side#2.2]]",
+      "links": [
+        {
+          "name": "Portal",
+          "link": "Side#2.2",
+          "pid": "15"
+        }
+      ],
+      "name": "Side#2",
+      "pid": "5",
+      "position": {
+        "x": "960",
+        "y": "314"
+      }
+    },
+    {
+      "text": "You see only one portal in front of you.\n\n[[Yellow Portal->Path#3]]",
+      "links": [
+        {
+          "name": "Yellow Portal",
+          "link": "Path#3",
+          "pid": "8"
+        }
+      ],
+      "name": "Path#2",
+      "pid": "6",
+      "position": {
+        "x": "828",
+        "y": "432"
+      }
+    },
+    {
+      "text": "This room has even more cobwebs and has a weird smell to it. Around you are three more portals.\n\n[[Purple Portal->Side#1.3]]\n[[Black Portal->Side#1.4]]\n[[Teal Portal->Side#1.5]]",
+      "links": [
+        {
+          "name": "Purple Portal",
+          "link": "Side#1.3",
+          "pid": "12"
+        },
+        {
+          "name": "Black Portal",
+          "link": "Side#1.4",
+          "pid": "13"
+        },
+        {
+          "name": "Teal Portal",
+          "link": "Side#1.5",
+          "pid": "14"
+        }
+      ],
+      "name": "Side#1.2",
+      "pid": "7",
+      "position": {
+        "x": "563",
+        "y": "313"
+      }
+    },
+    {
+      "text": "It seems to be a straight path to the next room.\n\n[[Green Portal->Path#4]]",
+      "links": [
+        {
+          "name": "Green Portal",
+          "link": "Path#4",
+          "pid": "9"
+        }
+      ],
+      "name": "Path#3",
+      "pid": "8",
+      "position": {
+        "x": "827",
+        "y": "551"
+      }
+    },
+    {
+      "text": "You are greeted with two portals.\n\n[[Blue Portal->Path#5]]\n[[White Portal->Side#3]]",
+      "links": [
+        {
+          "name": "Blue Portal",
+          "link": "Path#5",
+          "pid": "10"
+        },
+        {
+          "name": "White Portal",
+          "link": "Side#3",
+          "pid": "19"
+        }
+      ],
+      "name": "Path#4",
+      "pid": "9",
+      "position": {
+        "x": "826",
+        "y": "667"
+      }
+    },
+    {
+      "text": "You only see one other portal through this room, but throughthe portal you see what looks like a box on a table.\n\n[[Violet Portal->Finish Room]]",
+      "links": [
+        {
+          "name": "Violet Portal",
+          "link": "Finish Room",
+          "pid": "11"
+        }
+      ],
+      "name": "Path#5",
+      "pid": "10",
+      "position": {
+        "x": "826",
+        "y": "781"
+      }
+    },
+    {
+      "text": "Good Job!! You escaped!!",
+      "name": "Finish Room",
+      "pid": "11",
+      "position": {
+        "x": "826",
+        "y": "901"
+      }
+    },
+    {
+      "text": "This is just an empty room.",
+      "name": "Side#1.3",
+      "pid": "12",
+      "position": {
+        "x": "562",
+        "y": "431"
+      }
+    },
+    {
+      "text": "This room has nothing but big red text on the wall:\n\"TURN AROUND\"",
+      "name": "Side#1.4",
+      "pid": "13",
+      "position": {
+        "x": "449",
+        "y": "314"
+      }
+    },
+    {
+      "text": "This is just an empty room.",
+      "name": "Side#1.5",
+      "pid": "14",
+      "position": {
+        "x": "562",
+        "y": "203"
+      }
+    },
+    {
+      "text": "As the light above you wobbles around, you are greeted to two portals:\n\n[[Lime Portal->Side#2.3]]\n[[Brown Portal->Side#2.4]]",
+      "links": [
+        {
+          "name": "Lime Portal",
+          "link": "Side#2.3",
+          "pid": "16"
+        },
+        {
+          "name": "Brown Portal",
+          "link": "Side#2.4",
+          "pid": "17"
+        }
+      ],
+      "name": "Side#2.2",
+      "pid": "15",
+      "position": {
+        "x": "1075",
+        "y": "315"
+      }
+    },
+    {
+      "text": "There is another white portal in front of you.\n\n[[White Portal->Hint Room #2]]",
+      "links": [
+        {
+          "name": "White Portal",
+          "link": "Hint Room #2",
+          "pid": "18"
+        }
+      ],
+      "name": "Side#2.3",
+      "pid": "16",
+      "position": {
+        "x": "1076",
+        "y": "432"
+      }
+    },
+    {
+      "text": "This room seems to be empty and full of cobwebs.",
+      "name": "Side#2.4",
+      "pid": "17",
+      "position": {
+        "x": "1192",
+        "y": "316"
+      }
+    },
+    {
+      "text": "You walk into this room and see something written on the wall:\n\"ROYGBV\"",
+      "name": "Hint Room #2",
+      "pid": "18",
+      "position": {
+        "x": "1192",
+        "y": "434"
+      }
+    },
+    {
+      "text": "This room is dark and you feel cobwebs all over your body.\n\n[[Gray Portal->Side#3.2]]",
+      "links": [
+        {
+          "name": "Gray Portal",
+          "link": "Side#3.2",
+          "pid": "20"
+        }
+      ],
+      "name": "Side#3",
+      "pid": "19",
+      "position": {
+        "x": "942",
+        "y": "669"
+      }
+    },
+    {
+      "text": "You begin to smell a terrible smell as you progress into this room.\n\n[[Silver Portal->Side#3.3]]",
+      "links": [
+        {
+          "name": "Silver Portal",
+          "link": "Side#3.3",
+          "pid": "21"
+        }
+      ],
+      "name": "Side#3.2",
+      "pid": "20",
+      "position": {
+        "x": "1057",
+        "y": "669"
+      }
+    },
+    {
+      "text": "You enter a room that is full of trash and bugs, this room seems to serve no purpose as to progressing any further.",
+      "name": "Side#3.3",
+      "pid": "21",
+      "position": {
+        "x": "1169",
+        "y": "669"
+      }
+    }
+  ],
+  "name": "Project 1- Portal Maze",
+  "startnode": "1",
+  "creator": "Twine",
+  "creator-version": "2.3.9",
+  "ifid": "1C628809-CBE4-44D2-8559-8BA6B44431CD"
+}
